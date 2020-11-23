d1e15a56d9788bc218005b20b73dbfdedaa091e2
BobSalmon <baccatabob@users.noreply.github.com>
2020-11-23 09:30:29 +0000

    Add files via upload

--- advent/script3a.js
+++ advent/script3a.js
@@ -17,36 +17,10 @@ angular.module('adventCalendar', ['ngCookies']).controller('AdventCalendarContro
 function($scope, $window, $cookies, $sce) {
 
 $scope.doors = [];
-/*
-{day:1, opened:false, videoId:'nn5ken3RJBo'},
-{day:2, opened:false, videoId:'U_Tux6tixN0'},
-{day:3, opened:false, videoId:'Y7uhbiUs6eY'},
-{day:4, opened:false, videoId:'-7oJ9I2N3xk'},
-{day:5, opened:false, videoId:'7qQAE794uvo'},
-{day:6, opened:false, videoId:'IetPAANnhzQ'},
-{day:7, opened:false, videoId:'C6o0amydcLU'},
-{day:8, opened:false, videoId:'0j2JRcC6wBs'},
-{day:9, opened:false, videoId:'Wit-jGD4wCw'},
-{day:10, opened:false, videoId:'pliqObTHxUQ'},
-{day:11, opened:false, videoId:'yxDZjg_Igoc'},
-{day:12, opened:false, videoId:'h7QlxvTIU6Q'},
-{day:13, opened:false, videoId:'UbR5p34-grc'},
-{day:14, opened:false, videoId:'OxSJSZsD62g'},
-{day:15, opened:false, videoId:'sw5ZCZeS32M'},
-{day:16, opened:false, videoId:'AEyGZlBdkaA'},
-{day:17, opened:false, videoId:'ifXjjHWmOC4'},
-{day:18, opened:false, videoId:'QUzwkchlOw0'},
-{day:19, opened:false, videoId:'tFFv6ZePDG0'},
-{day:20, opened:false, videoId:'tue3g6tmys4'},
-{day:21, opened:false, videoId:'pwpxAtJS-Sc'},
-{day:22, opened:false, videoId:'2Bayi6ETui8'},
-{day:23, opened:false, videoId:'NaJ5FnM0Bzo'},
-{day:24, opened:false, videoId:'BIPTE84l9ls'}
-*/
+
 // cxqQtUQErhQ
 // j8N2YTikOsc
 // cjtYgGN3z6U
-//];
 
 $scope.today = '';
 
@@ -60,7 +34,7 @@ $scope.loadData = function() {
 	
 	for(var i=0; i<numDays; i++) {
 
-		$scope.doors.push({day: i+1, opened: false, videoId: videoIds[i]});
+		$scope.doors.push({day: i+1, position: Math.random(), opened: false, videoId: videoIds[i]});
 	}
 	
 	var alreadyOpened = $cookies.get(cookieName);
