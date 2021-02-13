d7df24a4223187ae9d0b80e9c2fd08cb4e493cac
BobSalmon <baccatabob@users.noreply.github.com>
2021-02-13 20:54:16 +0000

    First attempt at async
    From https://css-tricks.com/lazy-load-embedded-youtube-videos/

---
+++ birthday/script6.js
@@ -0,0 +1,111 @@
+function setDoorHeight() {
+
+	const ratio = 0.605;	// 121 / 200
+
+	let doors = document.getElementsByClassName('door');
+	let doorWidth = doors[0].offsetWidth;
+	let doorHeight = Math.floor(doorWidth * ratio) + 'px';
+	
+	for(var i=0; i < doors.length; i++) {
+		doors[i].style.height = doorHeight;
+	}
+}
+
+(function(angular) {
+  'use strict';
+angular.module('BirthdayApp', ['ngCookies']).controller('BirthdayController',
+function($scope, $window, $cookies, $sce) {
+
+$scope.doors = [];
+$scope.contents = [];
+
+$scope.today = '';
+$scope.startDate = '';
+$scope.timeSinceStart = '';
+$scope.latestActiveDoor = '';
+
+const numDays = 50;
+
+$scope.loadData = function() {
+	
+	$scope.today = new Date();
+	$scope.startDate = new Date("02/10/2021");
+	$scope.timeSinceStart = $scope.today.getTime() - $scope.startDate.getTime();
+	$scope.latestActiveDoor = Math.trunc($scope.timeSinceStart / (1000 * 3600 * 24)) + 1; 
+	
+	for(var i=0; i<numDays; i++) {
+
+		$scope.doors.push({day: i+1, position: Math.random(), opened: false, videoId: videoIds[i]});
+		$scope.contents[i+1] = '';
+	}
+	
+	var alreadyOpened = $cookies.get(cookieName);
+
+	var openedDoors = [];
+
+	if (alreadyOpened) {
+		var openedDoors = alreadyOpened.split(",");
+
+		angular.forEach(openedDoors, function(d) {
+			$scope.doors[d-1].opened = true;
+		});
+	}
+};
+
+$scope.extractOpenDoorNumbers = function () {
+	var openedDoors = [];
+
+	angular.forEach($scope.doors, function(d) {
+		if (d.opened) {
+			openedDoors.push(d.day);
+		}
+	});
+
+	var result = openedDoors.join();
+
+	return result;
+}
+
+$scope.openDoor = function(dayNum) {
+
+	if (dayNum > $scope.latestActiveDoor) {
+		return;
+	}
+	
+	$scope.doors[dayNum-1].opened = true;
+	var allOpenedDoors = $scope.extractOpenDoorNumbers();
+
+	var expireDate = new Date();
+	expireDate.setDate(expireDate.getDate() + 365);
+
+	$cookies.put(cookieName, allOpenedDoors, {'expires': expireDate});
+};
+
+$scope.getIframeSrc = function (dayNum) {
+	var videoId = $scope.doors[dayNum-1].videoId;
+	var fullUrl = 'https://www.youtube.com/embed/' + videoId;
+
+	return $sce.trustAsResourceUrl(fullUrl);
+};
+
+$scope.getIframeSrcDoc = function(dayNum) {
+	var videoId = $scope.doors[dayNum-1].videoId;
+
+	var contents = '<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>' +
+					'<a href="https://www.youtube.com/embed/' + videoId + '">' +
+					'<img src="https://img.youtube.com/vi/' + videoId + '/hqdefault.jpg><span> </span></a>';
+	
+	return $sce.trustAsHtml(contents);
+};
+
+
+$scope.random = function() {
+	return 0.5 - Math.random();
+}
+
+angular.element(function () {
+    setDoorHeight();
+});
+
+});
+})(window.angular);
